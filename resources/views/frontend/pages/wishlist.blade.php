8c3026a457d5e6241e4aabbdb79d0bbc8128a69e
Ismaila Jallow <ismailaj@cayorenterprises.com>
2022-04-20 23:45:37 +0000

    removed newsletter from cart and checkout as well as layout changes

--- resources/views/frontend/pages/wishlist.blade.php
+++ resources/views/frontend/pages/wishlist.blade.php
@@ -17,7 +17,7 @@
 		</div>
 	</div>
 	<!-- End Breadcrumbs -->
-			
+
 	<!-- Shopping Cart -->
 	<div class="shopping-cart section">
 		<div class="container">
@@ -29,8 +29,8 @@
 							<tr class="main-hading">
 								<th>PRODUCT</th>
 								<th>NAME</th>
-								<th class="text-center">TOTAL</th> 
-								<th class="text-center">ADD TO CART</th> 
+								<th class="text-center">TOTAL</th>
+								<th class="text-center">ADD TO CART</th>
 								<th class="text-center"><i class="ti-trash remove-icon"></i></th>
 							</tr>
 						</thead>
@@ -38,7 +38,7 @@
 							@if(Helper::getAllProductFromWishlist())
 								@foreach(Helper::getAllProductFromWishlist() as $key=>$wishlist)
 									<tr>
-										@php 
+										@php
 											$photo=explode(',',$wishlist->product['photo']);
 										@endphp
 										<td class="image" data-title="No"><img src="{{$photo[0]}}" alt="{{$photo[0]}}"></td>
@@ -51,7 +51,7 @@
 										<td class="action" data-title="Remove"><a href="{{route('wishlist-delete',$wishlist->id)}}"><i class="ti-trash remove-icon"></i></a></td>
 									</tr>
 								@endforeach
-							@else 
+							@else
 								<tr>
 									<td class="text-center">
 										There are no any wishlist available. <a href="{{route('product-grids')}}" style="color:blue;">Continue shopping</a>
@@ -69,9 +69,9 @@
 		</div>
 	</div>
 	<!--/ End Shopping Cart -->
-			
+
 	<!-- Start Shop Services Area  -->
-	<section class="shop-services section">
+	<section class="shop-services section mb-5">
 		<div class="container">
 			<div class="row">
 				<div class="col-lg-3 col-md-6 col-12">
@@ -114,11 +114,10 @@
 		</div>
 	</section>
 	<!-- End Shop Newsletter -->
-	
-	@include('frontend.layouts.newsletter')
-	
-	
-	
+
+
+
+
 	<!-- Modal -->
         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog">
             <div class="modal-dialog" role="document">
@@ -231,7 +230,7 @@
             </div>
         </div>
         <!-- Modal end -->
-	
+
 @endsection
 @push('scripts')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
