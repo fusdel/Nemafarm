086c176a2d6dc6f35e114b6c27dbe277dc86b5e4
Ismaila Jallow <ismailaj@cayorenterprises.com>
2022-04-28 12:27:24 +0000

    slight ui updates for blogs

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -26,7 +26,7 @@ class FrontendController extends Controller
 
     public function home(){
         $featured=Product::where('status','active')->where('is_featured',1)->orderBy('price','DESC')->limit(2)->get();
-        $posts=Post::where('status','active')->orderBy('id','DESC')->limit(1)->get();
+        $posts=Post::where('status','active')->orderBy('id','DESC')->limit(2)->get();
         $banners=Banner::where('status','active')->limit(3)->orderBy('id','DESC')->get();
         // return $banner;
         $products=Product::where('status','active')->orderBy('id','DESC')->limit(8)->get();
