95ba16805620294e07f074453f06edea043fabec
Ismaila Jallow <ismailaj@cayorenterprises.com>
2022-04-28 12:11:20 +0000

    slight ui updates for blogs

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -352,7 +352,10 @@
         <div class="row">
             @if($posts)
                 @foreach($posts as $post)
-                    <div class="col-lg-3 col-md-6 col-12">
+                    <div
+                    class={{(count($category_lists) === 1) ? 'col-12' :  (count($category_lists) === 2 ? 'col-6' : 'col-lg-4 col-md-12 col-12' )}}
+                    class="col-lg-3 col-md-6 col-12"
+                    >
                         <!-- Start Single Blog  -->
                         <div class="shop-single-blog">
                             <img src="{{$post->photo}}" alt="{{$post->photo}}">
